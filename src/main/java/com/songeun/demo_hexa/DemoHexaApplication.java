5506a4f1a9c421b37817527721c5115e5aa33b29
mungsil <thddmd2009@gmail.com>
2026-01-12 11:56:53 +0900

    setting: init boot application

---
+++ src/main/java/com/songeun/demo_hexa/DemoHexaApplication.java
@@ -0,0 +1,13 @@
+package com.songeun.demo_hexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHexaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHexaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/songeun/demo_hexa/DemoHexaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songeun.demo_hexa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHexaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
